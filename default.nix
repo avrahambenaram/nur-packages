6cc4c3d15cb1ca6b0ef3dbb3478c66ed57fcd2c7
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-26 11:04:54 -0300

    feat: add unimatrix package

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  unimatrix = pkgs.callPackage ./pkgs/unimatrix { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
