d3c5292dd9c235047357aac268fd94b87298f887
Avraham <107276491+avrahambenaram@users.noreply.github.com>
2024-01-26 11:26:24 -0300

    fix: unimatrix hash

--- pkgs/unimatrix/default.nix
+++ pkgs/unimatrix/default.nix
@@ -7,6 +7,7 @@ pkgs.stdenv.mkDerivation rec {
   src = pkgs.fetchgit {
     url = "https://github.com/will8211/unimatrix";
     name = "unimatrix";
+    hash = "sha256-QN2YunUN0nHD6aa4praWYy+o6lmPMFzR3rnVdEJw6fc=";
   };
 
   phases = "installPhase";
