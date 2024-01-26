8eb5e290e2ecc2757c5bf9f41ac25a2de3e165e3
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-26 11:09:31 -0300

    fix: remove undefined package

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
   unimatrix = pkgs.callPackage ./pkgs/unimatrix { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
