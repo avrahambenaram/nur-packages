99ea1be3400f3a175ca87c2869ea5e925aaa58bb
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-30 18:15:12 -0300

    fix: 1 - trying to fix nur error

--- pkgs/unimatrix/default.nix
+++ pkgs/unimatrix/default.nix
@@ -1,11 +1,11 @@
 { pkgs, stdenv, ... }:
 
 let
-  unimatrix = import (
+  unimatrix = (
     pkgs.fetchgit {
       url = "https://github.com/will8211/unimatrix";
       name = "unimatrix";
       hash = "sha256-QN2YunUN0nHD6aa4praWYy+o6lmPMFzR3rnVdEJw6fc=";
     }
-  ) { inherit pkgs; };
+  );
 in unimatrix
