5650c38ae2dc32d386f3864b4b86447546ed09d1
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-01-30 18:13:07 -0300

    feat: unimatrix derivation

--- pkgs/unimatrix/default.nix
+++ pkgs/unimatrix/default.nix
@@ -1,9 +1,11 @@
 { pkgs, stdenv, ... }:
 
-import (
-  pkgs.fetchgit {
-    url = "https://github.com/will8211/unimatrix";
-    name = "unimatrix";
-    hash = "sha256-QN2YunUN0nHD6aa4praWYy+o6lmPMFzR3rnVdEJw6fc=";
-  }
-) { inherit pkgs; }
+let
+  unimatrix = import (
+    pkgs.fetchgit {
+      url = "https://github.com/will8211/unimatrix";
+      name = "unimatrix";
+      hash = "sha256-QN2YunUN0nHD6aa4praWYy+o6lmPMFzR3rnVdEJw6fc=";
+    }
+  ) { inherit pkgs; };
+in unimatrix
